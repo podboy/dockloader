f1f12065f3a8fd9b99df90308183770720bbd407
Mingzhe Zou <zoumingzhe@qq.com>
2024-06-09 01:59:56 +0800

    add config diff

---
+++ scripts/config-diff.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+import sys
+from typing import List
+
+from dockloader import Tag
+from dockloader import TagConfig
+
+old_config_file = sys.argv[1]
+new_config_file = sys.argv[2]
+
+old_config = TagConfig(old_config_file)
+new_config = TagConfig(new_config_file)
+
+new_tags: List[Tag] = []
+for tag in new_config:
+    if tag not in old_config:
+        new_tags.append(tag)
+print(" ".join([tag.name for tag in new_tags]))
